3c93cc38165d54038ec100ea24be36e67059d60e
Amitesh Maurya <aamiteshmaurya@gmail.com>
2025-09-17 21:32:24 +0530

    update auth

--- src/app/api/admin/applications/route.ts
+++ src/app/api/admin/applications/route.ts
@@ -5,7 +5,7 @@ import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import  authOptions  from "@/lib/authOptions";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const session = await getServerSession(authOptions);
   if (!session || session.user.role !== "ADMIN") {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
